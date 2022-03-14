b1f5997f3e3ab7c120c6fc321603a5ea95d1dc8a
Napat <napat.wongchr@gmail.com>
2022-03-14 13:52:09 +0700

    add ex

---
+++ ex-1.js
@@ -0,0 +1,18 @@
+let studentProfile = {
+  name: "John",
+  age: 20,
+  scores: {
+    thai: 40,
+  },
+};
+
+let additionalProperties = {
+  photo: "https://placedog.net/500",
+  hobbies: ["Football", "Coding"],
+};
+
+let newStudentProfile = {};
+
+// Start coding here
+
+console.log(newStudentProfile);
---
+++ ex-2.js
@@ -0,0 +1,19 @@
+let studentProfile = {
+  name: "John",
+  age: 20,
+  scores: {
+    thai: 40,
+  },
+};
+
+let studentScores = {
+  math: 30,
+  english: 70,
+  tech: 100,
+};
+
+let newStudentProfile = {};
+
+// Start coding here
+
+console.log(newStudentProfile);
