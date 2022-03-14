53de1f06393cfd829c6e0dc9749ef3069a102da6
Napat <napat.wongchr@gmail.com>
2022-03-14 16:01:47 +0700

    update ex

---
+++ ex-3.js
@@ -0,0 +1,16 @@
+let orders = [
+  { orderId: "A001", paymentType: "Cash", totalPrice: 2500 },
+  { orderId: "A002", paymentType: "Credit Card", totalPrice: 4000 },
+  { orderId: "A003", paymentType: "Credit Card", totalPrice: 6000 },
+];
+
+let additionalOrders = [
+  { orderId: "A004", paymentType: "Credit Card", totalPrice: 611000 },
+  { orderId: "A005", paymentType: "Credit Card", totalPrice: 45000 },
+];
+
+let newOrders = [];
+
+// Start coding here
+
+console.log(newOrders);
---
+++ ex-4.js
@@ -0,0 +1,9 @@
+function sum() {
+  // Start coding here !
+}
+
+let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
+let result2 = sum(1, 2, 3, 4);
+
+console.log(`Result ${result1}`);
+console.log(`Result ${result2}`);
