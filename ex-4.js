47714ac9766766720a97f0b642fd321b868dbc18
Napat <napat.wongchr@gmail.com>
2022-03-14 22:23:49 +0700

    update ex

--- ex-4.js
+++ ex-4.js
@@ -1,9 +1,16 @@
-function sum() {
-  // Start coding here !
-}
+let studentProfile = {
+  name: "John",
+  age: 20,
+  scores: {
+    thai: 40,
+  },
+  hobbies: ["Football", "Coding"],
+};
 
-let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
-let result2 = sum(1, 2, 3, 4);
+let additionalHobbies = ["Painting", "Yoga"];
 
-console.log(`Result ${result1}`);
-console.log(`Result ${result2}`);
+// Start coding here
+
+let newStudentProfile = {};
+
+console.log(newStudentProfile);
