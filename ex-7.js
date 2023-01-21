d6b1f0bd6a35e95452cef4da4ab0b8cf02c1fe33
James <knotsite@gmail.com>
2023-01-21 15:25:44 +0700

    add ex 7 and test

---
+++ ex-7.js
@@ -0,0 +1,16 @@
+const employee1 = {
+  name: "John",
+  age: 20,
+};
+
+const result1 = employee1.scores.english;
+
+const employee2 = {
+  name: "A",
+  age: 50,
+  scores: {
+    math: 40,
+  },
+};
+
+const result2 = employee2.scores.english;
---
+++ test/ex-7.test.js
@@ -0,0 +1,23 @@
+import fs from "fs/promises";
+
+describe("exercise 7: Other Features tests case", () => {
+  test("โปรแกรมจะต้องไม่เกิด Error และเมื่อทำการ console.log ตัว `result1` จะได้ผลลัพธ์แสดงทางหน้าจอออกมาว่า undefined", async () => {
+    const data = await fs.readFile("./ex-7.js");
+    const code = `${data} return result1`;
+
+    const func = new Function(code);
+    const result = func();
+
+    expect(result).toBe(undefined);
+  });
+
+  test("โปรแกรมจะต้องไม่เกิด Error และเมื่อทำการ console.log ตัว `result2` จะได้ผลลัพธ์แสดงทางหน้าจอออกมาว่า 'English score does not defined'", async () => {
+    const data = await fs.readFile("./ex-7.js");
+    const code = `${data} return result2`;
+
+    const func = new Function(code);
+    const result = func();
+
+    expect(result).toBe("English score does not defined");
+  });
+});
