e2ac3ba754d2a09553171b1617fbeb1f26b3e62e
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-09-18 13:38:31 +0700

    fix:fix ex-6 testcase

--- test/ex-6.test.js
+++ test/ex-6.test.js
@@ -1,13 +1,22 @@
 import fs from "fs/promises";
+import { jest } from "@jest/globals";
 
 describe("exercise 6: Other Features tests case", () => {
+  beforeAll(() => {
+    jest.spyOn(console, "log");
+  });
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
   test("เมื่อทำการ console.log ตัว `result` จะได้ผลลัพธ์แสดงทางหน้าจอออกมาว่า 'Height is not defined'", async () => {
     const data = await fs.readFile("./ex-6.js");
     const code = `${data} return result`;
 
     const func = new Function(code);
     const result = func();
+    expect(console.log.mock.calls[0][0]).toBe("Height is not defined");
 
-    expect(result).toBe("Height is not defined");
   });
 });
+
